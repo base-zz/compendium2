08ba1cb44fa5cb7bdec8a59c6f9794f8048d20e5
Bassel Hajj <Bassel Hajj>
2025-04-02 12:13:03 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'compendiumnav',
+  webDir: 'dist'
+};
+
+export default config;
